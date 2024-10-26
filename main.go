cefed9ebcd2e33b71ca15036d0cb207e37f331fe
Eduardo Backes <128044560+EduardoBackesdev@users.noreply.github.com>
2024-10-26 04:20:06 -0300

    att, tratando erros

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	db "lib/src/lib_functions"
+)
+
+func main() {
+	db.ConnectionString("asf;a", "", "", "", "")
+
+}
--- src/lib_functions/connection.go
+++ src/lib_functions/connection.go
@@ -2,6 +2,7 @@ package lib_functions
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/lib/pq"
 )
@@ -28,16 +29,35 @@ func openConn(host, port, user, password, dbname string) (*sql.DB, error) {
 		panic(err) // TRATAR MELHOR O ERRO
 	}
 	err = db.Ping()
+
+	if err != nil {
+		fmt.Printf("erro:", err)
+		return db, err
+	}
 	return db, err
 }
 
-// Funcao exportada para lib aceitando Struct
+// Funcao de conexao ao banco esperando parametro como struct com os seguintes dados:
+//
+//	{
+//		host     string
+//		port     string
+//		user     string
+//		password string
+//		dbname   string
+//	}
 func ConnectionStruct(params sConnection) sdata {
 	res, err := openConn(params.host, params.port, params.user, params.password, params.dbname)
 	return sdata{res: res, err: err}
 }
 
-// Funcao exportada para lib aceitando Strings
+// Funcao de conexao ao banco esperando parametros com as seguintes strings:
+//
+//	host     string
+//	port     string
+//	user     string
+//	password string
+//	dbname   string
 func ConnectionString(host, port, user, password, dbname string) sdata {
 	res, err := openConn(host, port, user, password, dbname)
 	return sdata{res: res, err: err}
