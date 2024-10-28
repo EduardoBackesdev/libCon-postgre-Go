7ab398a51677a6122c6ee2cf65cd673a620c7e0f
Eduardo Backes <128044560+EduardoBackesdev@users.noreply.github.com>
2024-10-27 21:39:07 -0300

    Finalizando funcao de conexão com o banco postgree

--- src/lib_functions/connection.go
+++ src/lib_functions/connection.go
@@ -2,53 +2,30 @@ package lib_functions
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+	"log"
 
 	_ "github.com/lib/pq"
 )
 
-// Struct para retornar dados do tipo referente ao banco
-type sdata struct {
-	res *sql.DB
-	err error
-}
-
-// Struct para parametros da funcao de conexao do banco
-type sConnection struct {
-	host     string
-	port     string
-	user     string
-	password string
-	dbname   string
-}
-
 // Funcao principal de conexao com o banco de dados
 func openConn(host, port, user, password, dbname string) (*sql.DB, error) {
+	if host == "" || port == "" || user == "" || password == "" || dbname == "" {
+		errStrings := errors.New("Check the fields and try again!")
+		fmt.Errorf(errStrings.Error())
+		return nil, errStrings
+	}
 	db, err := sql.Open("postgres", "host="+host+" port="+port+" user="+user+" password="+password+" dbname="+dbname+" sslmode=disable")
 	if err != nil {
-		panic(err) // TRATAR MELHOR O ERRO
+		fmt.Errorf(err.Error())
+		return nil, err
 	}
 	err = db.Ping()
-
 	if err != nil {
-		fmt.Printf("erro:", err)
-		return db, err
+		log.Fatalln(err)
 	}
 	return db, err
-}
-
-// Funcao de conexao ao banco esperando parametro como struct com os seguintes dados:
-//
-//	{
-//		host     string
-//		port     string
-//		user     string
-//		password string
-//		dbname   string
-//	}
-func ConnectionStruct(params sConnection) sdata {
-	res, err := openConn(params.host, params.port, params.user, params.password, params.dbname)
-	return sdata{res: res, err: err}
 }
 
 // Funcao de conexao ao banco esperando parametros com as seguintes strings:
@@ -58,7 +35,7 @@ func ConnectionStruct(params sConnection) sdata {
 //	user     string
 //	password string
 //	dbname   string
-func ConnectionString(host, port, user, password, dbname string) sdata {
+func ConnectionString(host, port, user, password, dbname string) (*sql.DB, error) {
 	res, err := openConn(host, port, user, password, dbname)
-	return sdata{res: res, err: err}
+	return res, err
 }
