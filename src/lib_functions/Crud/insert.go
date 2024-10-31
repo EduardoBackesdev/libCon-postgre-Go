88039d5dfe92bd122aefaea05d901592f2b19785
Eduardo Backes <128044560+EduardoBackesdev@users.noreply.github.com>
2024-10-30 22:29:24 -0300

    att

--- src/lib_functions/Crud/insert.go
+++ src/lib_functions/Crud/insert.go
@@ -1,12 +1,26 @@
 package lib_functions
 
 import (
+	"errors"
+	"fmt"
 	db "lib/src/lib_functions/Connection"
 )
 
 // Funcao principal para query de insert
-func insertLib() {
-	db.OpenConn()
+func insertLib(table string, coluns []string, values []string) {
+	db, err := db.OpenConn()
+	if err != nil {
+		errString := errors.New("Error in connect!")
+		fmt.Errorf("Error", errString)
+		return
+	}
+	defer db.Close()
+	queryString := ""
+	_, errQuery := db.Exec()
+	if errQuery != nil {
+		fmt.Errorf("Error: ", errQuery)
+		return
+	}
 
 }
 
