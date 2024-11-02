a255eb2ece4383eaf5734c3a9ddc3ae6f88e554c
Eduardo Backes <eduardo.programacao@hotmail.com>
2024-11-02 16:59:55 -0300

    att new insert

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	lib_functions "lib/src/lib_functions/Connection"
+)
+
+func main() {
+	lib_functions.ConnectionConfig()
+
+}
--- src/lib_functions/Crud/insert.go
+++ src/lib_functions/Crud/insert.go
@@ -4,19 +4,26 @@ import (
 	"errors"
 	"fmt"
 	db "lib/src/lib_functions/Connection"
+	"strings"
 )
+
+// type okStruct struct {
+// 	Message string
+// 	status  int
+// 	Err     Error()
+// }
 
 // Funcao principal para query de insert
 func insertLib(table string, coluns []string, values []string) {
 	db, err := db.OpenConn()
 	if err != nil {
-		errString := errors.New("Error in connect!")
+		errString := errors.New("Error in connect")
 		fmt.Errorf("Error", errString)
 		return
 	}
 	defer db.Close()
-	queryString := ""
-	_, errQuery := db.Exec()
+	queryString := "INSER INTO" + table + "(" + strings.Join(coluns, ",") + ") VALUES (" + strings.Join(values, ",") + ")"
+	_, errQuery := db.Exec(queryString)
 	if errQuery != nil {
 		fmt.Errorf("Error: ", errQuery)
 		return
@@ -24,6 +31,6 @@ func insertLib(table string, coluns []string, values []string) {
 
 }
 
-func Insert() {
-
+func Insert(t string, c []string, v []string) {
+	insertLib(t, c, v)
 }
